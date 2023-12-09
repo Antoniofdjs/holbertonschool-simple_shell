9e7d94700b76c20eacca3ef05bd2b1602e0f5aa3
Antoniofdjs <Antoniofdjs@gmail.com>
2023-12-09 02:01:53 -0400

    made get_tokens function and the fucntion now does the array for us

--- my_shell.c
+++ my_shell.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <string.h>
+#include "main.h"
 int main ()
 {
 	char *str_line = NULL;
 	size_t len = 0;
 	ssize_t read_bytes;
-	int on = 1, tok_count = 0, i;
-	char *args[20];
-	char *token;
+	int on = 1;
+	char **args;
 
 	while (on != 0)
 	{
@@ -33,19 +30,8 @@ int main ()
 				on = 0;
 			else
 			{
-				token = strtok(str_line, " "); /* make tokens */
-				tok_count = 0;
-				while (token != NULL)
-				{
-					args[tok_count] = strdup(token); /* dup tokens into args */
-					tok_count++;
-					token = strtok(NULL, " "); /* next token */
-				}
-				for (i = 0; i < tok_count; i++)/* print tokens */
-				{
-					printf("%s\n", args[i]);
-					free(args[i]);
-				}
+				args = get_tokens(str_line); /* makes tokens and args array with mallocs */
+				execve(args[0], args, NULL);
 			}
 		}
 	}
