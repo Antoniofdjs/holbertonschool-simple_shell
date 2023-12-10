e5df236c4eb59b2282b0e4b36d50834d70350381
Antoniofdjs <Antoniofdjs@gmail.com>
2023-12-09 20:08:38 -0400

    added free to args for the entire array that it receives from get_token

--- my_shell.c
+++ my_shell.c
@@ -5,7 +5,7 @@ int main ()
 	char *str_line = NULL;
 	size_t len = 0;
 	ssize_t read_bytes;
-	int on = 1;
+	int on = 1, i = 0;
 	char **args;
 
 	while (on != 0)
@@ -13,7 +13,7 @@ int main ()
 		printf("($) ");
 		read_bytes = getline(&str_line, &len, stdin); /* stdin into str_line */
 
-		if(read_bytes == -1)
+		if(read_bytes == -1) /* check if getline is succesfull */
 		{
 			printf("\n");
 			on = 0;
@@ -26,15 +26,19 @@ int main ()
 				read_bytes--; /* -1 to the old \n byte count */
 			}
 
-			if (strcmp(str_line, "Exit") == 0)
+			if (strcmp(str_line, "Exit") == 0) /* check if user put Exit */
 				on = 0;
 			else
 			{
 				args = get_tokens(str_line); /* makes tokens and args array with mallocs */
 				execve(args[0], args, NULL);
+				
+				for (i = 0; args[i] != NULL; i++) /* free the array and indxs */
+					free(args[i]);
+				free(args);
 			}
 		}
 	}
-	free(str_line);
+	free(str_line);/* always free the str_line form stdin */
 	return (0);
 }
