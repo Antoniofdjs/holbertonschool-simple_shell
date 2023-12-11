d1df471594d300c981028938dd48f56e852ce757
Antoniofdjs <Antoniofdjs@gmail.com>
2023-12-10 21:12:58 -0400

    Acces function added before excecve, also non-interactive mode in process isatty

--- my_shell.c
+++ my_shell.c
@@ -8,37 +8,55 @@ int main ()
 	int on = 1, i = 0;
 	char **args;
 
-	while (on != 0)
+	if (!isatty(fileno(stdin))) /* non-interactive mode */
 	{
-		printf("($) ");
-		read_bytes = getline(&str_line, &len, stdin); /* stdin into str_line */
-
-		if(read_bytes == -1) /* check if getline is succesfull */
+		while((read_bytes = getline(&str_line, &len, stdin)) != -1) /* stdin into str_line */
 		{
-			printf("\n");
-			on = 0;
-		}
-		else
-		{
-			if (str_line[read_bytes - 1] == '\n')
+			if(read_bytes == -1) /* check if getline is succesfull */
 			{
-				str_line[read_bytes - 1] = '\0'; /* removed \n with \0 */
-				read_bytes--; /* -1 to the old \n byte count */
-			}
-
-			if (strcmp(str_line, "Exit") == 0) /* check if user put Exit */
+				printf("\n");
 				on = 0;
-			else
-			{
-				args = get_tokens(str_line); /* makes tokens and args array with mallocs */
-				execve(args[0], args, NULL);
-				
-				for (i = 0; args[i] != NULL; i++) /* free the array and indxs */
-					free(args[i]);
-				free(args);
 			}
+
+			printf("%s",str_line);
 		}
 	}
+	else /* interactive mode */
+		while (on != 0)
+		{
+			printf("($) ");
+			read_bytes = getline(&str_line, &len, stdin); /* stdin into str_line */
+
+			if(read_bytes == -1) /* check if getline is succesfull */
+			{
+				printf("\n");
+				on = 0;
+			}
+			else
+			{
+				if (str_line[read_bytes - 1] == '\n')
+				{
+					str_line[read_bytes - 1] = '\0'; /* removed \n with \0 */
+					read_bytes--; /* -1 to the old \n byte count */
+				}
+
+				if (strcmp(str_line, "exit") == 0) /* check if user put Exit */
+					on = 0;
+				else
+				{
+					args = get_tokens(str_line); /* makes tokens and args array with mallocs */
+
+					if (access(args[0], X_OK) != 0) /* finds if file is excutable */
+						printf("-bash: %s: command not found\n",args[0]);
+					else
+						execve(args[0], args, NULL);
+
+					for (i = 0; args[i] != NULL; i++) /* free the array and indxs */
+						free(args[i]);
+					free(args);
+				}
+			}
+		}
 	free(str_line);/* always free the str_line form stdin */
 	return (0);
 }
