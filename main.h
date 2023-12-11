d0454807fcf16c6511684376a9cda7fcf919ece7
Livanhernandez <livanhernandez9@gmail.com>
2023-12-10 22:36:23 -0400

    added sys/stat and sys/wait for fork and environ

---
+++ main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+char **get_tokens(char *str_line);
+#endif
