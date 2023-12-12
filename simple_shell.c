cd34a1b472d2b5b53a04105b3aaf91c4baf4a831
Juan C Lopez <juancalpz@yahoo.com>
2023-12-12 14:16:58 -0400

    Implemented the get_path function

--- simple_shell.c
+++ simple_shell.c
@@ -8,50 +8,23 @@ int main ()
 	char *str_line = NULL;
 	size_t len = 0;
 	ssize_t read_bytes;
-	int on = 1, i = 0, spaces;
+	int on = 1, i = 0;
 	char **args;
 
 	if (!isatty(fileno(stdin))) /* non-interactive mode */
 	{
 		while((read_bytes = getline(&str_line, &len, stdin)) != -1) /* stdin into str_line */
 		{
-
-			remove_newline(&str_line, &read_bytes);
-			if (strcmp(str_line, "exit") == 0) /* check if user put Exit */
-				on = 0;
-			else
+			if(read_bytes == -1) /* check if getline is succesfull */
 			{
-				args = get_tokens(str_line); /* makes tokens and args array with mallocs */
-				if (args[0] == NULL)/* args might be whte spaces only */
-				{
-					free(str_line);
-					free(args);
-					return (main());
-				}
-				if (access(args[0], X_OK) != 0) /* finds if file is excutable */
-				{
-					for (i = 0; args[0][i] != '\0'; i++)
-					{
-						if(args[0][i] != ' ')
-						{
-							spaces = 0;
-							break;	
-						}
-					}
-					if (spaces == 0)
-						printf("-bash: %s: command not found\n",args[0]);
-				}
-				else
-					my_exe(args, environ);
-
-				for (i = 0; args[i] != NULL; i++) /* free the array and indxs */
-					free(args[i]);
-				free(args);
+				printf("\n");
+				on = 0;
 			}
+
+			printf("%s",str_line);
 		}
 	}
 	else /* interactive mode */
-	{
 		while (on != 0)
 		{
 			printf("($) ");
@@ -64,32 +37,35 @@ int main ()
 			}
 			else
 			{
-				remove_newline(&str_line, &read_bytes);
+				if (str_line[read_bytes - 1] == '\n')
+				{
+					str_line[read_bytes - 1] = '\0'; /* removed \n with \0 */
+					read_bytes--; /* -1 to the old \n byte count */
+				}
+
 				if (strcmp(str_line, "exit") == 0) /* check if user put Exit */
 					on = 0;
 				else
 				{
 					args = get_tokens(str_line); /* makes tokens and args array with mallocs */
-					if (args[0] == NULL) /* if args was null, maybe white spaces only */
-					{
-						free(str_line);
-						free(args);
-						return (main());
-					}
-					if (access(args[0], X_OK) != 0) /* finds if file is excutable */
-					{
-						printf("not found command\n");
-					}
-					else
-						my_exe(args, environ);
 
+					if (args != NULL)
+					{
+						if (access(args[0], X_OK) == 0)
+							my_exe(args, environ);
+						else
+						{
+							get_path(args, environ);
+						}
+					}
+					if (access(args[0], X_OK) != 0)
+						printf("-bash: %s: command not found\n", args[0]);
 					for (i = 0; args[i] != NULL; i++) /* free the array and indxs */
 						free(args[i]);
 					free(args);
 				}
 			}
 		}
-	}
 	free(str_line);/* always free the str_line form stdin */
 	return (0);
 }
