093ecbd858966b076929ee612a57a71949339984
Livanhernandez <livanhernandez9@gmail.com>
2023-12-12 15:46:48 -0400

    free args function file

---
+++ free_args.c
@@ -0,0 +1,10 @@
+#include "main.h"
+#include <stdio.h>
+void free_args(char **args)
+{
+	int i = 0;
+
+	for (i = 0; args[i] != NULL; i++) /* free the array and indxs */
+		free(args[i]);
+	free(args);
+}
