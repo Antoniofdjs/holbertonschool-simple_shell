e581865f45e01bb758833d90d22863e34715422a
Livanhernandez <livanhernandez9@gmail.com>
2023-12-12 15:46:14 -0400

    added prototype to free args

--- main.h
+++ main.h
@@ -6,4 +6,8 @@
 #include <sys/stat.h>
 #include <sys/wait.h>
 char **get_tokens(char *str_line);
+void my_exe(char **args, char **environ);
+void remove_newline(char **str_line, ssize_t *read_bytes);
+void get_path(char **full_path, char **environ);
+void free_args(char **args);
 #endif
