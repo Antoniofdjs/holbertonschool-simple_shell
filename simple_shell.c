5783f724203965de77c73c9d6573b22543887026
Livanhernandez <livanhernandez9@gmail.com>
2023-12-12 16:04:30 -0400

    running non interactive and interactive shell

--- simple_shell.c
+++ simple_shell.c
@@ -15,13 +15,29 @@ int main ()
 	{
 		while((read_bytes = getline(&str_line, &len, stdin)) != -1) /* stdin into str_line */
 		{
-			if(read_bytes == -1) /* check if getline is succesfull */
-			{
-				printf("\n");
+			remove_newline(&str_line, &read_bytes);
+			if (strcmp(str_line, "exit") == 0) /* check if user put Exit */
 				on = 0;
+			else
+			{
+				args = get_tokens(str_line); /* makes tokens and args array with mallocs */
+				if (args[0] == NULL) /* if white spaces only */
+				{
+					free(str_line);
+					free(args);
+					return (main());
+				}
+				if (args != NULL)
+				{
+					if (access(args[0], X_OK) == 0)
+						my_exe(args, environ);
+					else
+						get_path(args, environ);
+				}
+				if (access(args[0], X_OK) != 0)
+					printf("-bash: %s: command not found\n", args[0]);
+				free_args(args);	
 			}
-
-			printf("%s",str_line);
 		}
 	}
 	else /* interactive mode */
@@ -37,18 +53,18 @@ int main ()
 			}
 			else
 			{
-				if (str_line[read_bytes - 1] == '\n')
-				{
-					str_line[read_bytes - 1] = '\0'; /* removed \n with \0 */
-					read_bytes--; /* -1 to the old \n byte count */
-				}
-
+				remove_newline(&str_line, &read_bytes);
 				if (strcmp(str_line, "exit") == 0) /* check if user put Exit */
 					on = 0;
 				else
 				{
 					args = get_tokens(str_line); /* makes tokens and args array with mallocs */
-
+					if (args[0] == NULL) /* if white spaces only */
+					{
+						free(str_line);
+						free(args);
+						return (main());
+					}
 					if (args != NULL)
 					{
 						if (access(args[0], X_OK) == 0)
