b58e9e66c82a84779498e1a0d45bd6e562cecc08
Livanhernandez <livanhernandez9@gmail.com>
2023-12-12 16:42:26 -0400

    TEST FOR CHECKERS

--- remove_newline.c
+++ remove_newline.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 #include "main.h"
+/**
+ * remove_newline - removes new line.
+ * @str_line: 
+ * @read_bytes: total chars read for str_line
+ */
 
 void remove_newline(char **str_line, ssize_t *read_bytes)
 {
