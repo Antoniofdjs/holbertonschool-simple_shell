b50e9c9b80271d7b1ee034a38383eec4911f7b68
Livanhernandez <livanhernandez9@gmail.com>
2023-12-12 16:27:57 -0400

    TEST FOR CHECKERS

--- free_args.c
+++ free_args.c
@@ -1,5 +1,10 @@
 #include "main.h"
 #include <stdio.h>
+/**
+ * free_args - free the args array
+ * @args : array from get tokens, tokenization of str_line
+ */
+
 void free_args(char **args)
 {
 	int i = 0;
