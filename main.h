32f96d9410b75ef41b6b29708f9eb5be3b8cef86
Antoniofdjs <Antoniofdjs@gmail.com>
2023-12-13 11:09:00 -0400

    TEST CHECK & BETTY

--- main.h
+++ main.h
@@ -5,9 +5,12 @@
 #include <unistd.h>
 #include <sys/stat.h>
 #include <sys/wait.h>
+extern char **environ;
 char **get_tokens(char *str_line);
 void my_exe(char **args, char **environ);
 void remove_newline(char **str_line, ssize_t *read_bytes);
 void get_path(char **full_path, char **environ);
 void free_args(char **args);
+int white_spaces(char **args, char *str_line);
+int main(void);
 #endif
