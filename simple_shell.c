4caa99985c310113f5c42816128b2abd995bb15f
Antoniofdjs <Antoniofdjs@gmail.com>
2023-12-13 11:17:02 -0400

    TEST CHECK & BETTY

--- simple_shell.c
+++ simple_shell.c
@@ -2,10 +2,11 @@
 #include "main.h"
 
 /**
+ * main - simple system shell
+ * Return: 0 on succes
  */
-extern char **environ;
 
-int main ()
+int main(void)
 {
 	char *str_line = NULL;
 	size_t len = 0;
@@ -13,73 +14,38 @@ int main ()
 	int on = 1;
 	char **args;
 
-	if (!isatty(fileno(stdin))) /* non-interactive mode */
+	while (on != 0)
 	{
-		while((read_bytes = getline(&str_line, &len, stdin)) != -1) /* stdin into str_line */
+		if (isatty(fileno(stdin)))
+			printf("($) ");
+		read_bytes = getline(&str_line, &len, stdin); /* stdin into str_line */
+
+		if (read_bytes == -1) /* check if getline is succesfull */
 		{
-			remove_newline(&str_line, &read_bytes); /* removes newline */
+			if (isatty(fileno(stdin)))
+				printf("\n");
+			on = 0;
+		}
+		else
+		{
+			remove_newline(&str_line, &read_bytes);
 			if (strcmp(str_line, "exit") == 0) /* check if user put Exit */
 				on = 0;
 			else
 			{
 				args = get_tokens(str_line); /* makes tokens and args array with mallocs */
-				if (args[0] == NULL) /* if white spaces only */
-				{
-					free(str_line);
-					free(args);
+				if ((white_spaces(args, str_line)) == 0) /* if white spaces only */
 					return (main());
-				}
-				if (args != NULL)
-				{
-					if (access(args[0], X_OK) == 0)
-						my_exe(args, environ);
-					else
-						get_path(args, environ);
-				}
+				if (access(args[0], X_OK) == 0) /*!args */
+					my_exe(args, environ);
+				else
+					get_path(args, environ); /*!args */
 				if (access(args[0], X_OK) != 0)
 					printf("-bash: %s: command not found\n", args[0]);
-				free_args(args);	
+				free_args(args);
 			}
 		}
 	}
-	else /* interactive mode */
-		while (on != 0)
-		{
-			printf("($) ");
-			read_bytes = getline(&str_line, &len, stdin); /* stdin into str_line */
-
-			if(read_bytes == -1) /* check if getline is succesfull */
-			{
-				printf("\n");
-				on = 0;
-			}
-			else
-			{
-				remove_newline(&str_line, &read_bytes);
-				if (strcmp(str_line, "exit") == 0) /* check if user put Exit */
-					on = 0;
-				else
-				{
-					args = get_tokens(str_line); /* makes tokens and args array with mallocs */
-					if (args[0] == NULL) /* if white spaces only */
-					{
-						free(str_line);
-						free(args);
-						return (main());
-					}
-					if (args != NULL)
-					{
-						if (access(args[0], X_OK) == 0)
-							my_exe(args, environ);
-						else
-							get_path(args, environ);
-					}
-					if (access(args[0], X_OK) != 0)
-						printf("-bash: %s: command not found\n", args[0]);
-					free_args(args);
-				}
-			}
-		}
 	free(str_line);/* always free the str_line form stdin */
 	return (0);
 }
