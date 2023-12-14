2c39db29cf249c08cdd6f0fd72f2c14f38f43c36
Antoniofdjs <Antoniofdjs@gmail.com>
2023-12-13 21:51:07 -0400

    TEST CHECK PATH

--- functions.c
+++ functions.c
@@ -53,77 +53,77 @@ char **env_path(char **environ)
 
 char **get_tokens(char *str_line)
 {
-        char **array;
-        int tok_count = 0;
-        char *token, *str_line_dup;
+	char **array;
+	int tok_count = 0;
+	char *token, *str_line_dup;
 
-        str_line_dup = strdup(str_line);/* we need dup to count tokens */
-        token = strtok(str_line_dup, " ");
+	str_line_dup = strdup(str_line);/* we need dup to count tokens */
+	token = strtok(str_line_dup, " ");
 
-        while (token != NULL)
-        {
-                tok_count++;
-                token = strtok(NULL, " "); /* next token */
-        }
+	while (token != NULL)
+	{
+		tok_count++;
+		token = strtok(NULL, " "); /* next token */
+	}
 
-        free(str_line_dup);
-        array = malloc((tok_count + 1) * sizeof(char *));/* making size for indexes */
+	free(str_line_dup);
+	array = malloc((tok_count + 1) * sizeof(char *));/* making size for indexes */
 
-        if (array == NULL)
-                return (NULL);
+	if (array == NULL)
+		return (NULL);
 
-        token = strtok(str_line, " "); /* original str to tokenize */
-        tok_count = 0;
+	token = strtok(str_line, " "); /* original str to tokenize */
+	tok_count = 0;
 
-        while (token != NULL)
-        {
-                array[tok_count] = strdup(token); /* dup tokens into args,does malloc also*/
-                tok_count++;
-                token = strtok(NULL, " "); /* next token */
-        }
-        array[tok_count] = NULL;/* last index of args */
-        return (array); /* sending array, args in main receives it */
+	while (token != NULL)
+	{
+		array[tok_count] = strdup(token); /* dup tokens into args,does malloc also*/
+		tok_count++;
+		token = strtok(NULL, " "); /* next token */
+	}
+	array[tok_count] = NULL;/* last index of args */
+	return (array); /* sending array, args in main receives it */
 }
 
 /**
- * get_path - pairs args[0] with possible paths
+ * get_path - Concat args[0] with possible paths
  * @args : array from tokenization of get_tokens of str_line
  * @environ : golbal variable from system
  */
 
 void get_path(char **args, char **environ)
 {
-        char **path;
-        char *full_path;
-        int i, did_path = 0;
+	char **path;
+	char *full_path;
+	int i, did_path = 0;
 
-        path = env_path(environ);
-        for (i = 0; path[i] != NULL; i++) /*iterate path and concatenate with args[0]*/
-        {
-                full_path = malloc(strlen(path[i]) + strlen(args[0]) + 1);
+	path = env_path(environ);
+	for (i = 0; path[i] != NULL; i++) /*iterate path and concat args[0]*/
+	{
+		full_path = malloc(strlen(path[i]) + strlen(args[0]) + 1);
 
-                if (full_path == NULL) /* verify if malloc is succesful */
-                {
-                        perror("Memory allocation error");
-                        exit(EXIT_FAILURE);
-                }
-                strcpy(full_path, path[i]);
-                strcat(full_path, args[0]);
-                if (access(full_path, X_OK) == 0) /* verify if path is executable */
-                {
-                        free(args[0]);
-                        args[0] = strdup(full_path);
-                        my_exe(args, environ);
-                        free(full_path);
-                        free_array(path);
-                        did_path = 1;
-                        break;
-                }
-                free(full_path); /* done the full path, args[0] has full now */
+		if (full_path == NULL) /* verify if malloc is succesful */
+		{
+			perror("Memory allocation error");
+			exit(EXIT_FAILURE);
+		}
+		strcpy(full_path, path[i]);
+		strcat(full_path, args[0]);
+		if (access(full_path, X_OK) == 0) /* verify if path is executable */
+		{
+			free(args[0]);
+			args[0] = strdup(full_path);
+			my_exe(args, environ);
+			free(full_path);
+			free_array(path);
+			did_path = 1;
+			break;
+		}
+		free(full_path); /* done the full path, args[0] has full now */
 
-        }
-                if (did_path == 0)
-                        free_array(path);
+	}
+	if (did_path == 0)
+		free_array(path);
 }
 
 /**
@@ -134,24 +134,24 @@ void get_path(char **args, char **environ)
 
 void my_exe(char **args, char **environ)
 {
-        pid_t pid;
+	pid_t pid;
 
-        pid = fork();
+	pid = fork();
 
-        if (pid == -1)/* child wasn't process and it couldn't work */
-        {
-                perror("fork");
-                exit(EXIT_FAILURE);
-        }
-        if (pid == 0)/* child process is the current process in work */
-        {
-                execve(args[0], args, environ);
-                perror("execve");
-                exit(EXIT_FAILURE);
-        }
-        else
-        {
-                wait(NULL);/* parent process suspended until the child finishes */
-        }
+	if (pid == -1)/* child wasn't process and it couldn't work */
+	{
+		perror("fork");
+		exit(EXIT_FAILURE);
+	}
+	if (pid == 0)/* child process is the current process in work */
+	{
+		execve(args[0], args, environ);
+		perror("execve");
+		exit(EXIT_FAILURE);
+	}
+	else
+	{
+		wait(NULL);/* parent process suspended until the child finishes */
+	}
 
 }
